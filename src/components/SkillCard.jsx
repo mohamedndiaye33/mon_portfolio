41a0679fe920ad0d888bec90cf3018af7396446d
Mohamedndiaye33 <mohamedndiaye3575@gmail.com>
2026-03-10 17:33:42 +0000

    Mon portfolio

--- src/components/SkillCard.jsx
+++
@@ -1,9 +0,0 @@
-function SkillCard({ skill }) {
-  return (
-    <div className="bg-gray-100 dark:bg-gray-700 p-3 rounded shadow text-center">
-      {skill}
-    </div>
-  );
-}
-
-export default SkillCard;
